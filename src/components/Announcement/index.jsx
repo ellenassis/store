67645d1ddea1cb74eac50028291b364721b355a6
ellenassis <ellenassis8@gmail.com>
2023-02-28 16:56:09 -0300

    feat: add Announcement component

---
+++ src/components/Announcement/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./style.scss";
+
+export const Announcement = () => {
+  return (
+    <div className="container">
+      <p>Frete grátis para pedidos acima de R$150,00</p>
+    </div>
+  );
+};
