17597bb2f90c5d5b5aef25266137510512311c57
ellenassis <ellenassis8@gmail.com>
2023-10-12 17:19:07 -0300

    feat: add Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template lang="">
+  <div class="bg-light flex flex-col justify-between items-center gap-8 pt-10 px-10 rounded-3xl lg:flex-row">
+    <div class="text-center lg:text-start">
+      <h1 class="mb-6 font-bold">Your Fashion Journey Begins Here</h1>
+      <p>
+        Shop Now and Save Big with 60% Off!
+      </p>
+    </div>
+    <div>
+      <img class="max-w-max" src="../assets/images/hero.png" alt="" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+<style lang=""></style>
