ac895b911d77f4527ea8647165a0661155c66eab
ellenassis <ellenassis8@gmail.com>
2023-10-12 17:18:16 -0300

    add the base configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
+export const content = [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+];
+export const theme = {
+  colors: {
+    'main-color': '#F06C9B',
+    'secondary': '#F5D491',
+    'light': '#F9B9B7',
+    'white': '#faf9f9',
+    'dark': '#555b6e',
   },
-  plugins: [],
-}
+  fontFamily: {
+    
+  },
+  extend: {
+    borderRadius: {
+      '4xl': '2rem',
+    }
+  },
+};
+export const plugins = [];
 
