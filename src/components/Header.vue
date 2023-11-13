e7871651cb0c30a10973d1d77b07ca3f8ee548e7
ellenassis <ellenassis8@gmail.com>
2023-11-13 20:10:33 -0300

    feat: add header section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,19 @@
 <template lang="">
-  <div class="flex justify-between items-center py-10 relative">
-    <div>
-      <span class="text-4xl font-bold text-main-color"><a href="#">Muar.</a></span>
-    </div>
-    <div class="absolute left-0 right-0">
-      <ul class="flex justify-center gap-8 font-medium">
-        <li class="text-main-color font-bold"><a href="#">Home</a></li>
-        <li><a href="#">Store</a></li>
-        <li><a href="#">Blog</a></li>
-      </ul>
-    </div>
-    <div>
-      <img src='../assets/images/cart.png' class='cursor-pointer' />
+  <div class="bg-white fixed z-10 w-full left-0 right-0">
+    <div class="header relative flex justify-between items-center py-6 px-4">
+      <div>
+        <span class="text-4xl font-bold text-main-color"><a href="#">Muar.</a></span>
+      </div>
+      <div class="absolute right-0 left-0">
+        <ul class="flex justify-center gap-8 font-medium">
+          <li class="text-main-color font-bold"><a href="#">Home</a></li>
+          <li><a href="#">Store</a></li>
+          <li><a href="#">Blog</a></li>
+        </ul>
+      </div>
+      <div>
+        <img src="../assets/images/cart.png" class="cursor-pointer" />
+      </div>
     </div>
   </div>
 </template>
@@ -20,4 +22,9 @@ export default {
   name: 'Header'
 }
 </script>
-<style lang=""></style>
+<style>
+.header {
+  max-width: 1400px;
+  margin: 0 auto;
+}
+</style>
