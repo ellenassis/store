2b8d3805a565bb28562e4d79efc51d4f5e9eb62e
ellenassis <ellenassis8@gmail.com>
2023-11-13 20:10:57 -0300

    update hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,13 +1,15 @@
 <template lang="">
-  <div class="bg-light flex flex-col justify-between items-center gap-8 pt-10 px-10 rounded-3xl lg:flex-row">
-    <div class="text-center lg:text-start">
-      <h1 class="mb-6 font-bold">Your Fashion Journey Begins Here</h1>
-      <p>
-        Shop Now and Save Big with 60% Off!
-      </p>
-    </div>
-    <div>
-      <img class="max-w-max" src="../assets/images/hero.png" alt="" />
+  <div class="bg-hero pt-10 px-10">
+    <div
+      class="relative page-content hero flex flex-col justify-between items-center gap-8 lg:flex-row"
+    >
+      <div class="text-center lg:text-start">
+        <h1 class="mb-6 uppercase">Fashion for lifestyle.</h1>
+        <p>Shop Now and Save Big with 60% Off!</p>
+      </div>
+      <div>
+        <img class="absolute bottom-0 right-0 w-3/5" src="../assets/images/woman-hero.png" alt="" />
+      </div>
     </div>
   </div>
 </template>
@@ -16,4 +18,14 @@ export default {
   name: 'Hero'
 }
 </script>
-<style lang=""></style>
+<style>
+.hero {
+  min-height: 70vh;
+}
+.bg-hero {
+  background-image: url('../assets/images/bg-hero.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  object-fit: cover;
+}
+</style>
