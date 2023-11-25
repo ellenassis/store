8e60dcefac49f570fd9e33c85d30f1ffeed5470b
ellenassis <ellenassis8@gmail.com>
2023-11-25 13:33:01 -0300

    create Home view

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import HeaderGlobal from './components/HeaderGlobal.vue';
+import Home from './views/Home.vue';
 </script>
 
-<template>
-
-
-  <RouterView />
+<template class="app">
+  <HeaderGlobal />
+  <Home /> 
 </template>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <HeroGlobal />
+    <ShoppingBag />
+    <div class="page-content">
+      <ProductCatalog />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import HeroGlobal from '../components/HeroGlobal.vue'
+import ShoppingBag from '../components/ShoppingBag.vue'
+import ProductCatalog from '../components/ProductCatalog.vue'
+
+export default {
+  components: {
+    HeroGlobal,
+    ShoppingBag,
+    ProductCatalog
+  }
+}
+</script>
