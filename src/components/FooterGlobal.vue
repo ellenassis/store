9e5c93d25f4e37c40840ad7739ddac08f019b2b8
ellenassis <ellenassis8@gmail.com>
2024-01-12 19:32:19 -0300

    add footer

--- src/components/FooterGlobal.vue
+++ src/components/FooterGlobal.vue
@@ -1,16 +1,22 @@
 <template lang="">
-<div class="footer">
-	<div class="footer-content">
-		<p>a</p>
+<div class="footer py-4 mt-10">
+	<div class="footer-content text-center">
+		<p>© {{currentYear}} | Ellen Penteado de Assis</p>
 	</div>
 </div>
 </template>
+<script lang="ts">
+export default {
+	data() {
+		return {
+			currentYear: new Date().getFullYear()
+		}
+	}
+}
+</script>
 <style>
 .footer {
 	background: #F9B9B7;
-}
-.footer-content {
-	max-width: 1400px;
 }
 </style>
   
